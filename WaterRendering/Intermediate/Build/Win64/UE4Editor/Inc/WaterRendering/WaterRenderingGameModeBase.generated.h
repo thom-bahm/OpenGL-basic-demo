a61bc2be5bcbf97fb15311b9dd452bf18b6a93fa
Thomas <56136360+thom-bahm@users.noreply.github.com>
2020-11-17 19:06:37 -0600

    Stuff

---
+++ WaterRendering/Intermediate/Build/Win64/UE4Editor/Inc/WaterRendering/WaterRenderingGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef WATERRENDERING_WaterRenderingGameModeBase_generated_h
+#error "WaterRenderingGameModeBase.generated.h already included, missing '#pragma once' in WaterRenderingGameModeBase.h"
+#endif
+#define WATERRENDERING_WaterRenderingGameModeBase_generated_h
+
+#define WaterRendering_Source_WaterRendering_WaterRenderingGameModeBase_h_15_SPARSE_DATA
+#define WaterRendering_Source_WaterRendering_WaterRenderingGameModeBase_h_15_RPC_WRAPPERS
+#define WaterRendering_Source_WaterRendering_WaterRenderingGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define WaterRendering_Source_WaterRendering_WaterRenderingGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAWaterRenderingGameModeBase(); \
+	friend struct Z_Construct_UClass_AWaterRenderingGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AWaterRenderingGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/WaterRendering"), NO_API) \
+	DECLARE_SERIALIZER(AWaterRenderingGameModeBase)
+
+
+#define WaterRendering_Source_WaterRendering_WaterRenderingGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAWaterRenderingGameModeBase(); \
+	friend struct Z_Construct_UClass_AWaterRenderingGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AWaterRenderingGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/WaterRendering"), NO_API) \
+	DECLARE_SERIALIZER(AWaterRenderingGameModeBase)
+
+
+#define WaterRendering_Source_WaterRendering_WaterRenderingGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AWaterRenderingGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AWaterRenderingGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AWaterRenderingGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AWaterRenderingGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AWaterRenderingGameModeBase(AWaterRenderingGameModeBase&&); \
+	NO_API AWaterRenderingGameModeBase(const AWaterRenderingGameModeBase&); \
+public:
+
+
+#define WaterRendering_Source_WaterRendering_WaterRenderingGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AWaterRenderingGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AWaterRenderingGameModeBase(AWaterRenderingGameModeBase&&); \
+	NO_API AWaterRenderingGameModeBase(const AWaterRenderingGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AWaterRenderingGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AWaterRenderingGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AWaterRenderingGameModeBase)
+
+
+#define WaterRendering_Source_WaterRendering_WaterRenderingGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define WaterRendering_Source_WaterRendering_WaterRenderingGameModeBase_h_12_PROLOG
+#define WaterRendering_Source_WaterRendering_WaterRenderingGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	WaterRendering_Source_WaterRendering_WaterRenderingGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	WaterRendering_Source_WaterRendering_WaterRenderingGameModeBase_h_15_SPARSE_DATA \
+	WaterRendering_Source_WaterRendering_WaterRenderingGameModeBase_h_15_RPC_WRAPPERS \
+	WaterRendering_Source_WaterRendering_WaterRenderingGameModeBase_h_15_INCLASS \
+	WaterRendering_Source_WaterRendering_WaterRenderingGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define WaterRendering_Source_WaterRendering_WaterRenderingGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	WaterRendering_Source_WaterRendering_WaterRenderingGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	WaterRendering_Source_WaterRendering_WaterRenderingGameModeBase_h_15_SPARSE_DATA \
+	WaterRendering_Source_WaterRendering_WaterRenderingGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	WaterRendering_Source_WaterRendering_WaterRenderingGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	WaterRendering_Source_WaterRendering_WaterRenderingGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> WATERRENDERING_API UClass* StaticClass<class AWaterRenderingGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID WaterRendering_Source_WaterRendering_WaterRenderingGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
