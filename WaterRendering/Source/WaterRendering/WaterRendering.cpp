b1a8622eb96f654cf37cb08d27ba847ba5890ccb
Thomas <56136360+thom-bahm@users.noreply.github.com>
2020-11-17 19:28:08 -0600

    delete

--- WaterRendering/Source/WaterRendering/WaterRendering.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "WaterRendering.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WaterRendering, "WaterRendering" );
